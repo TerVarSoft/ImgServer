95c8fea916a13f0451428f4067bc4a06b60ce0cb
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2016-09-20 22:54:04 -0400

    https

---
+++ server.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = 'production';
+
+var app = require('./app');
+
+var fs = require('fs'),
+https = require('https');
+
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  passphrase: 'test'
+}, app).listen(5000);
